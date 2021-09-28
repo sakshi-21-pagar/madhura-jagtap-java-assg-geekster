7b774d84c408ed9f735af249ef2f5a2e5f91f1b5
madhura-24 <71561605+madhura-24@users.noreply.github.com>
2021-09-28 23:13:47 +0530

    Add files via upload

---
+++ import.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Largenum
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 
+int a=40, b=78, c=19;  
+
+if(a>=b && a>=c)  
+System.out.println(a+" is the largest Number");  
+
+else if (b>=a && b>=c)  
+System.out.println(b+" is the largest Number");  
+else  
+ 
+System.out.println(c+" is the largest number");  
+	}
+}
---
+++ public class Even_odd.java
@@ -0,0 +1,15 @@
+public class Even_odd{
+
+    public static void main(String args[]){
+		int n = 40;
+		if (n % 2 == 0) {
+			System.out.println("It is even");
+		} else
+ {
+			System.out.println("It is odd");
+		}
+
+	}
+
+}
+
